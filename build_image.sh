517471e6b203c9db0d6cab20180f3f2515365539
Piers Harding <piers@catalyst.net.nz>
2018-06-06 17:14:42 +1200

    whats in bin/

--- build_image.sh
+++ build_image.sh
@@ -11,18 +11,20 @@ RUN curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
 RUN mkdir -p /results ${WORKDIR}
 COPY . ${WORKDIR}
 WORKDIR ${WORKDIR}
-CMD /usr/bin/make deps  && /usr/bin/make build && cp bin/* /results/
+CMD /usr/bin/make deps  && /usr/bin/make build && ls -l bin/ && cp bin/* /results/
 
 EOF
 docker build -t ${IMG} -f Dockerfile.builder .
 
 # /builds/piersharding/nfsshare-controller
 echo "Build ${BIN} in docker"
-docker run --name gobuilder -it ${IMG}
+docker run --name gobuilder \
+   -v /builds/piersharding/nfsshare-controller/bin:/results \
+   -it ${IMG}
 echo "Finished build."
 echo "Does the container still exist:"
 docker ps -a
-echo "Copy out ${BIN}"
-docker cp gobuilder:/results/${BIN} bin/
+#echo "Copy out ${BIN}"
+#docker cp gobuilder:/results/${BIN} bin/
 echo "Output:"
 ls -latr bin/
